fd61983a1fdbde06e2f63d240f0b166bc4846e4d
unknown <ferri-bcn96@hotmail.es>
2021-08-11 19:29:33 +0200

    New: add cache into autoCompleteLocationService

--- src/infrastructure/services/autoCompleteLocationService/index.js
+++ src/infrastructure/services/autoCompleteLocationService/index.js
@@ -1,12 +1,20 @@
+const predictionsKeyValue = [];
+
 async function getPlacePredictions(value) {
 
     if(!value || value === ""){
         return []
     }
 
+    if(predictionsKeyValue[value]){
+        return predictionsKeyValue[value];
+    }
+
     const service = new window.google.maps.places.AutocompleteService();
     const predictionsResults = await service.getPlacePredictions({ input: value });
     console.log(predictionsResults);
+    
+    predictionsKeyValue[value] = predictionsResults.predictions;
 
     return predictionsResults.predictions;
 
